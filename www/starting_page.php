e0f2da6ee7e2bfa56d866a0fd8e1f04665c4929f
s3926080-RMIT <95405783+s3926080-RMIT@users.noreply.github.com>
2022-09-11 17:58:46 +0700

    Delete starting_page.php

--- www/starting_page.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    $_SESSION['cartArray'] = array();
-    $_SESSION['orderArray'] = array();
-    $_SESSION['distributionHub'] = array();
-    $_SESSION['distributionHub'] = file("distribution.txt");
-?>
