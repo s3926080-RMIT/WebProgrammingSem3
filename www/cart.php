865e97265b4a6d3b1335919de35c09e30632e2d1
s3926080-RMIT <95405783+s3926080-RMIT@users.noreply.github.com>
2022-09-11 18:17:48 +0700

    Fixed inconsistencies in each file

---
+++ www/cart.php
@@ -0,0 +1,86 @@
+<?php include_once "header.php" ?>
+<!DOCTYPE html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Shopping Cart</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='cart.css'>
+    <script type="text/javascript" src="cart2.js"></script>
+</head>
+<h1>Shopping Cart</h1>
+
+<div class='shopping-cart'>
+<?php
+  $cartList = $_SESSION['cartArray'];
+
+  if (isset($_POST['order'])){
+    if (!empty($cartList)){
+      createOrder();
+    } 
+  }
+?>
+  <form method='post' action='thankyou.php'>
+    <?php
+      $total_money = 0;
+      for ($i = 0; $i < count($cartList); $i++){
+        echo "<div class='product'>";
+        echo "<div class='prodImg'>";
+        $str = $cartList[$i][3];
+        echo "<img src=product_images/$str>";
+        echo "</div>";
+        echo "<div class='prodName'>";
+        echo $cartList[$i][1];
+        echo "</div>";
+        echo "<div class='prodDescription'>";
+        echo $cartList[$i][5];
+        echo "</div>";
+        echo "<div class='prodPrice'>";
+        echo $cartList[$i][2];
+        echo "</div>";
+        echo "<div class='prodQuant'>";
+        echo "<input type='number' name='quantity[]' value='1' min='1'>";
+        echo "</div>";
+        echo "</div>";
+        echo "<div class='prodIndTotal'>";
+        echo $cartList[$i][2];
+        echo "</div>";
+        echo "</div>";
+        if (isset($_POST['quantity'])){
+          $total_money += $cartList[$i][2] * $_POST['quantity'][$i];
+        }
+          
+      }
+    ?>
+    <div class="totals">
+      <div class="totalItems trueTotal">
+        <label>Total</label>
+        <div class="totalValue" id="total"><?php echo $total_money ?></div>
+      </div>
+    </div>
+    <div class="orderBtnContainer">
+      <input class="orderBtn" type='submit' name='order' value='Order'>
+    </div>
+  </form>
+
+
+<?php
+  function createOrder(){
+    $orderFile = fopen("orders.txt", "a");
+    $orderList = $_SESSION['cartArray'];
+    for ($k = 0; $k < count($orderList); $k++){
+      //          product ID                product name              product quantity             product price              product image
+      $text = $orderList[$k][0] . "|" . $orderList[$k][1] . "|" .  $_POST['quantity'][$k] . "|" . $orderList[$k][2] . "|" . "<img src=product_images/" . $orderList[$k][3] . ">" . "\n";
+      fwrite($orderFile, $text . "");
+    }
+
+    fwrite($orderFile, "-----------\n");
+    
+    fclose($orderFile);
+  }
+
+  
+?>
+</div>
+
+<?php include_once "footer.php" ?>
---
+++ www/userprofile.php
@@ -0,0 +1,64 @@
+<?php
+	require_once('header.php');
+	?>
+		<nav class="navtop">
+		</nav>
+		<div class="content">
+			<center><h2>Profile Page</h2></center>
+			<br>
+			<div>	
+				<center><table>
+					<tr>
+						<td>Username: </td>
+						<td>
+							<?php
+						if(isset($_SESSION["username"])) {
+							$username = $_SESSION["username"];
+							echo "$username";
+						}
+						?>
+						</td>
+					</tr>
+					<tr>
+						<td>Name:</td>
+						<td>
+							<?php
+						if(isset($_SESSION["name"])) {
+							$name = $_SESSION["name"];
+							echo "$name";
+						}
+						?>
+						</td>
+					</tr>
+                    <tr>
+						<td>Address:</td>
+						<td>
+							<?php
+						if(isset($_SESSION["address"])) {
+							$address = $_SESSION["address"];
+							echo "$address";
+						}
+						?>
+						</td>
+					</tr>
+					<tr>
+						<td>Profile Picture:</td>
+						<td>
+							<?php
+						if(isset($_SESSION["file"])) {
+							$address = $_SESSION["profilepic"];
+							echo "$address";
+						}
+						?>
+						</td>
+					</tr>
+				</table></center>
+			</div>
+		</div>	
+	</body>
+	<br>
+	</br>
+	<?php
+	require_once('footer.php');
+	?>
+</html>
